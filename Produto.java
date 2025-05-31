7776204ec672d6ff6692c0c1c9c7b5846965675b
Laura <laura.contact02@gmail.com>
2025-05-31 14:30:09 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Produto {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    private int id;
+    private String nome;
+    private double preco;
+    private String categoria;
+
+    public Produto() { }
+
+    public Produto(int id, String nome, double preco, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    // getters e setters
+}
